2d71098bb668b3178be1b8b1f3b51dcc6d83930c
arogya <arogyabichpuria16@gmail.com>
2022-03-06 16:52:57 +0530

    futile tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/Spinner.jsx"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': 'Noto Sans, sans-serif',
+      }
+    },
+  },
+  plugins: [],
+}
